28f22559eaa6a46563f6cc586e231f65e0fdd078
apple <apple@gmail.com>
2026-07-30 10:40:45 +0530

    Initial Commit

---
+++ medora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  medora
+//
+//  Created by STUDENT_24 on 30/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ medora/medoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  medoraApp.swift
+//  medora
+//
+//  Created by STUDENT_24 on 30/07/26.
+//
+
+import SwiftUI
+
+@main
+struct medoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
